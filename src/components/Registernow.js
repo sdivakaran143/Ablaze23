ba5c9edf73abab48264b53d5872f6369fde83938
sdivakaran143 <sdivakaran683@gmail.com>
2023-09-16 16:13:17 +0530

    register now link  altered

--- src/components/Registernow.js
+++ src/components/Registernow.js
@@ -7,7 +7,7 @@ export default function Registernow(){
   return (
     <div className={styles.registerbar}>
       {/* <Button variant="contained" color="error" style={{marginRight:'10px'}}><a href="#089765">Schedule</a></Button> */}
-      <Button variant="contained" color="error"><Link target="_blank" to="https://forms.gle/44Wost3hpBsbo3oo8">Register Now</Link></Button>
+      <Button variant="contained" color="error"><Link target="_blank" to="https://forms.gle/AV2cJAdwFyhj8jFYA">Register Now</Link></Button>
     </div>
   )
 }
